0fa5ec8658a87c76118da29e7a39a113b4b2904a
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-07-21 16:38:15 +0900

    create form in new & create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :category_id, :color_id, :season_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([
+  { name: 'トップス' },
+  { name: 'スカート' },
+  { name: 'ワンピース' },
+  { name: 'パンツ' },
+  { name: 'アウター' },
+  { name: 'バッグ' },
+  { name: 'シューズ' }
+  ])
+Color.create([
+  { name: 'ブラック' },
+  { name: 'ホワイト' },
+  { name: 'グレー' },
+  { name: 'ベージュ' },
+  { name: 'ブラウン' },
+  { name: 'レッド' },
+  { name: 'ピンク' },
+  { name: 'ブルー' },
+  { name: 'イエロー' },
+  { name: 'グリーン' },
+  { name: 'パープル' }
+])
+Season.create([
+  { name: '春' },
+  { name: '夏' },
+  { name: '秋' },
+  { name: '冬' }
+])
