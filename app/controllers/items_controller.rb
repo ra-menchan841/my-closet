bbb3169ca09a524e10505a591e7c8e0d24e21d13
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-07-24 10:44:19 +0900

    edit view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,39 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :create_categoryTags, :countItems, only: [:index, :new]
+
   def index
-    @categoryTags = Category.all
-
-    tagNumber = 0
-    @amounts = []
-    while tagNumber < 8 do
-      tagNumber += 1
-      amount = Item.where(category_id: tagNumber).length
-      @amounts << amount
-    end
-    
-
   end
 
   def new
-    @categoryTags = Category.all
     @item = Item.new
-    
-    tagNumber = 0
-    @amounts = []
-    while tagNumber < 8 do
-      tagNumber += 1
-      amount = Item.where(category_id: tagNumber).length
-      @amounts << amount
-    end
-
-
   end
 
   def create
-    Item.create(item_params)
+    @image = Item.create(item_params)
     redirect_to root_path
   end
 
   private
+  def create_categoryTags
+    @categoryTags = Category.all
+  end
+
+  def countItems
+    tagNumber = 0
+    @amounts = []
+      while tagNumber < 8 do
+        tagNumber += 1
+        amount = Item.where(category_id: tagNumber).length
+        @amounts << amount
+      end
+  end
+
   def item_params
     params.require(:item).permit(:image, :category_id, :color_id, :season_id)
   end
