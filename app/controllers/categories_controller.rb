b26ba673c34dbf4d7566a972e248db4abbab4ab0
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-07-27 23:01:35 +0900

    edit view of category show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,22 @@
 class CategoriesController < ApplicationController
+  before_action :set_categories, :set_amounts, only: [:show]
   def show
-    @items = Items.find(params[:id])
+    @items = Item.where(category_id: params[:id])
   end
+
+  private
+  def set_categories
+    @categories = Category.all
+  end
+
+  def set_amounts
+    @amounts = []
+    number = 0
+    while number < 7 do
+      number += 1
+      @amount = Item.where(category_id: number).count
+      @amounts << @amount
+    end
+  end
+
 end
