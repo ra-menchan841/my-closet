1602f7cbc9dd6015febbd3601177d3de4635aed0
ra-menchan841 <yayo-piano0312@ezweb.ne.jp>
2020-08-01 12:56:30 +0900

    edit views of login & form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/users', to: redirect("/users/sign_up")
   root "items#index"
   resources :items, only: [:index, :new, :create, :show, :destroy]
   resources :categories, only: [:show]
